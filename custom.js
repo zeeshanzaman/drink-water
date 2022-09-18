cfad6fd36fe9b811e3044340c22a79c8bf5edecc
zeeshanzaman <88842812+zeeshanzaman@users.noreply.github.com>
2022-09-19 02:05:24 +0500

    add Event Listener highlight cups and make  function highlightCups

--- custom.js
+++ custom.js
@@ -4,5 +4,25 @@ const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remaind')
 
 smallCups.forEach((cup, idx) => {
-    cup.addEventListener('click', () => hightlightCups(idx))
+    cup.addEventListener('click', () => highlightCups(idx))
 })
+
+
+// it is made for click after click to empty the glass
+
+function highlightCups(idx) {
+    if (smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+        idx--
+    }
+
+
+    // make if else condition of highlightcups to add or remove class
+
+    smallCups.forEach((cup, idx2) => {
+        if (idx2 <= idx) {
+            cup.classList.add('full')
+        } else {
+            cup.classList.remove('full')
+        }
+    })
+}
