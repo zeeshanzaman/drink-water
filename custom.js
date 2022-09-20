7251ec021eb7b6ea3f789346020175e44a340b8e
zeeshanzaman <88842812+zeeshanzaman@users.noreply.github.com>
2022-09-20 13:03:26 +0500

    remained styling through js

--- custom.js
+++ custom.js
@@ -3,7 +3,6 @@ const listers = document.getElementById('lister')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remaind')
 
-updateBigCup()
 
 smallCups.forEach((cup, idx) => {
     cup.addEventListener('click', () => highlightCups(idx))
@@ -27,18 +26,27 @@ function highlightCups(idx) {
             cup.classList.remove('full')
         }
     })
+
+    updateBigCup()
 }
 
 function updateBigCup() {
     const fullCups = document.querySelectorAll('.cup-small.full').length
     const totalCups = smallCups.length
-
+        // it is for filling in larger cup
     if (fullCups === 0) {
         percentage.style.visibility = 'hidden'
         percentage.style.height = 0
     } else {
         percentage.style.visibility = 'visible'
         percentage.style.height = `${fullCups / totalCups * 330}px`
+            // it is for filling in larger cup writing 
+        percentage.innerText = `${fullCups / totalCups * 100}%`
+
+        if (fullCups === totalCups) {
+            remained.style.visibility = 'hidden'
+            remained.style.height = 0
+        }
     }
 
 
